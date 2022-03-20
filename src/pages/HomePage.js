91cd6e4a804d8e11ead6edbde960418abee30d95
Trình Huỳnh Thiên <trinh.huynhcollector@hcmut.edu.vn>
2022-03-20 13:37:04 +0700

    footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,20 @@
 import React from 'react';
 export default class HomePage extends React.Component {
-    constructor(props){
-        super(props);
-    }
+
 
     render() {
         return (
-            <h1>This is body</h1>
+            <div>
+                <h1>This is body</h1>
+                <h1>This is body</h1>
+                <h1>This is body</h1>
+                <h1>This is body</h1>
+                <h1>This is body</h1>
+                <h1>This is body</h1>
+                <h1>This is body</h1>
+                <h1>This is body</h1>
+            </div>
+            
         );
     }
 }
