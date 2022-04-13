656f58ab3bfbfde95e487ebe22405547063de865
Trình Huỳnh Thiên <trinh.huynhcollector@hcmut.edu.vn>
2022-04-13 14:20:26 +0700

    product info

--- src/pages/ProductPage/ProductInfoSection.js
+++ src/pages/ProductPage/ProductInfoSection.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import Box from '@material-ui/core/Box'
 import {useParams} from "react-router-dom"
 import productData from '../../productData'
-
+import Button from '@mui/material/Button';
 export default function ProductInfoSection() {
     const {productId} = useParams()
     const thisProduct = productData.find(prod => prod.id === productId)
+
     return (     
         <div>
             <h1>{thisProduct.name}</h1>
-            <p>Giá: {thisProduct.price}đ</p>
+            <h2>{thisProduct.price}đ trên mét</h2>
             <p>{thisProduct.description}</p>
+            <h4>Chất liệu: {thisProduct.material}</h4> 
+            <h4>Xuất xứ: {thisProduct.origin}</h4>
+            <h4>Chiều rộng: {thisProduct.width}cm</h4>
+            <h4>Cân nặng: {thisProduct.weight}gsm</h4>
+            <Button variant="contained" sx={{p:2, backgroundColor:"#384257"}}>
+                Thêm vào giỏ hàng
+            </Button>
+            
         </div>
     );
 }
--- src/productData.js
+++ src/productData.js
@@ -4,7 +4,7 @@ export default [
     {
     id: "1",
     name: "Vải a ",
-    description: "blah blah blah",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum leo ex, non ullamcorper velit placerat ut. Nunc varius urna ultrices arcu faucibus, ut fringilla magna suscipit. Sed ac eleifend nunc, vel maximus magna. Ut iaculis dolor vel convallis convallis. Phasellus ex quam, lobortis non varius eget, viverra vitae urna. Etiam convallis nunc diam, euismod commodo erat venenatis vitae. Morbi vestibulum sagittis varius. Ut volutpat ex eu purus pulvinar, sed congue nisi tempus. Nam sed metus id quam elementum malesuada quis ullamcorper enim. Cras pulvinar eleifend vestibulum. Morbi dapibus euismod eros quis malesuada. Maecenas eget commodo neque, ac mattis velit.",
     material: "Cotton",
     origin : "China",
     width : "143",
@@ -15,7 +15,7 @@ export default [
   {
     id: "2",
     name: "Vải b ",
-    description: "blah blah blah",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum leo ex, non ullamcorper velit placerat ut. Nunc varius urna ultrices arcu faucibus, ut fringilla magna suscipit. Sed ac eleifend nunc, vel maximus magna. Ut iaculis dolor vel convallis convallis. Phasellus ex quam, lobortis non varius eget, viverra vitae urna. Etiam convallis nunc diam, euismod commodo erat venenatis vitae. Morbi vestibulum sagittis varius. Ut volutpat ex eu purus pulvinar, sed congue nisi tempus. Nam sed metus id quam elementum malesuada quis ullamcorper enim. Cras pulvinar eleifend vestibulum. Morbi dapibus euismod eros quis malesuada. Maecenas eget commodo neque, ac mattis velit.",
     material: "Cotton",
     origin : "China",
     width : "143",
@@ -26,7 +26,7 @@ export default [
   {
     id: "3",
     name: "Vải c ",
-    description: "blah blah blah",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum leo ex, non ullamcorper velit placerat ut. Nunc varius urna ultrices arcu faucibus, ut fringilla magna suscipit. Sed ac eleifend nunc, vel maximus magna. Ut iaculis dolor vel convallis convallis. Phasellus ex quam, lobortis non varius eget, viverra vitae urna. Etiam convallis nunc diam, euismod commodo erat venenatis vitae. Morbi vestibulum sagittis varius. Ut volutpat ex eu purus pulvinar, sed congue nisi tempus. Nam sed metus id quam elementum malesuada quis ullamcorper enim. Cras pulvinar eleifend vestibulum. Morbi dapibus euismod eros quis malesuada. Maecenas eget commodo neque, ac mattis velit.",
     material: "Cotton",
     origin : "China",
     width : "143",
@@ -37,7 +37,7 @@ export default [
   {
     id: "4",
     name: "Vải d ",
-    description: "blah blah blah",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum leo ex, non ullamcorper velit placerat ut. Nunc varius urna ultrices arcu faucibus, ut fringilla magna suscipit. Sed ac eleifend nunc, vel maximus magna. Ut iaculis dolor vel convallis convallis. Phasellus ex quam, lobortis non varius eget, viverra vitae urna. Etiam convallis nunc diam, euismod commodo erat venenatis vitae. Morbi vestibulum sagittis varius. Ut volutpat ex eu purus pulvinar, sed congue nisi tempus. Nam sed metus id quam elementum malesuada quis ullamcorper enim. Cras pulvinar eleifend vestibulum. Morbi dapibus euismod eros quis malesuada. Maecenas eget commodo neque, ac mattis velit.",
     material: "Cotton",
     origin : "China",
     width : "143",
@@ -48,7 +48,7 @@ export default [
   {
     id: "5",
     name: "Vải e ",
-    description: "blah blah blah",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum leo ex, non ullamcorper velit placerat ut. Nunc varius urna ultrices arcu faucibus, ut fringilla magna suscipit. Sed ac eleifend nunc, vel maximus magna. Ut iaculis dolor vel convallis convallis. Phasellus ex quam, lobortis non varius eget, viverra vitae urna. Etiam convallis nunc diam, euismod commodo erat venenatis vitae. Morbi vestibulum sagittis varius. Ut volutpat ex eu purus pulvinar, sed congue nisi tempus. Nam sed metus id quam elementum malesuada quis ullamcorper enim. Cras pulvinar eleifend vestibulum. Morbi dapibus euismod eros quis malesuada. Maecenas eget commodo neque, ac mattis velit.",
     material: "Cotton",
     origin : "China",
     width : "143",
@@ -56,5 +56,48 @@ export default [
     color: "đỏ",
     price: "20000"
   },
-
+  {
+    id: "6",
+    name: "Vải e ",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum leo ex, non ullamcorper velit placerat ut. Nunc varius urna ultrices arcu faucibus, ut fringilla magna suscipit. Sed ac eleifend nunc, vel maximus magna. Ut iaculis dolor vel convallis convallis. Phasellus ex quam, lobortis non varius eget, viverra vitae urna. Etiam convallis nunc diam, euismod commodo erat venenatis vitae. Morbi vestibulum sagittis varius. Ut volutpat ex eu purus pulvinar, sed congue nisi tempus. Nam sed metus id quam elementum malesuada quis ullamcorper enim. Cras pulvinar eleifend vestibulum. Morbi dapibus euismod eros quis malesuada. Maecenas eget commodo neque, ac mattis velit.",
+    material: "Cotton",
+    origin : "China",
+    width : "143",
+    weight : "194",
+    color: "đỏ",
+    price: "20000"
+  },
+  {
+    id: "7",
+    name: "Vải e ",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum leo ex, non ullamcorper velit placerat ut. Nunc varius urna ultrices arcu faucibus, ut fringilla magna suscipit. Sed ac eleifend nunc, vel maximus magna. Ut iaculis dolor vel convallis convallis. Phasellus ex quam, lobortis non varius eget, viverra vitae urna. Etiam convallis nunc diam, euismod commodo erat venenatis vitae. Morbi vestibulum sagittis varius. Ut volutpat ex eu purus pulvinar, sed congue nisi tempus. Nam sed metus id quam elementum malesuada quis ullamcorper enim. Cras pulvinar eleifend vestibulum. Morbi dapibus euismod eros quis malesuada. Maecenas eget commodo neque, ac mattis velit.",
+    material: "Cotton",
+    origin : "China",
+    width : "143",
+    weight : "194",
+    color: "đỏ",
+    price: "20000"
+  },
+  {
+    id: "8",
+    name: "Vải e ",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum leo ex, non ullamcorper velit placerat ut. Nunc varius urna ultrices arcu faucibus, ut fringilla magna suscipit. Sed ac eleifend nunc, vel maximus magna. Ut iaculis dolor vel convallis convallis. Phasellus ex quam, lobortis non varius eget, viverra vitae urna. Etiam convallis nunc diam, euismod commodo erat venenatis vitae. Morbi vestibulum sagittis varius. Ut volutpat ex eu purus pulvinar, sed congue nisi tempus. Nam sed metus id quam elementum malesuada quis ullamcorper enim. Cras pulvinar eleifend vestibulum. Morbi dapibus euismod eros quis malesuada. Maecenas eget commodo neque, ac mattis velit.",
+    material: "Cotton",
+    origin : "China",
+    width : "143",
+    weight : "194",
+    color: "đỏ",
+    price: "20000"
+  },
+  {
+    id: "9",
+    name: "Vải e ",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse elementum leo ex, non ullamcorper velit placerat ut. Nunc varius urna ultrices arcu faucibus, ut fringilla magna suscipit. Sed ac eleifend nunc, vel maximus magna. Ut iaculis dolor vel convallis convallis. Phasellus ex quam, lobortis non varius eget, viverra vitae urna. Etiam convallis nunc diam, euismod commodo erat venenatis vitae. Morbi vestibulum sagittis varius. Ut volutpat ex eu purus pulvinar, sed congue nisi tempus. Nam sed metus id quam elementum malesuada quis ullamcorper enim. Cras pulvinar eleifend vestibulum. Morbi dapibus euismod eros quis malesuada. Maecenas eget commodo neque, ac mattis velit.",
+    material: "Cotton",
+    origin : "China",
+    width : "143",
+    weight : "194",
+    color: "đỏ",
+    price: "20000"
+  },
   ];
