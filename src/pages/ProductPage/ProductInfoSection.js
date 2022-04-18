4ee14f73f3c23ca9ac36993a7ca7d28b3f58af3e
Trình Huỳnh Thiên <trinh.huynhcollector@hcmut.edu.vn>
2022-04-18 13:12:20 +0700

    done product info

--- src/pages/ProductPage/ProductInfoSection.js
+++ src/pages/ProductPage/ProductInfoSection.js
@@ -6,10 +6,11 @@ import { useState } from "react";
 import { AddBox, IndeterminateCheckBox, Close } from "@mui/icons-material";
 import { TableCell, Paper, Table, TableRow, TableHead, TableBody, TableContainer, Typography, IconButton, InputBase } from "@mui/material";
 import { styled } from "@mui/material";
-function UnitSelect({ number, setNumber }) {
+function UnitSelect({ number, setNumber}) {
+
     return (
         <React.Fragment>
-            <IconButton sx={{ color: '#4e5b73' }} component="span" onClick={() => number && setNumber(number - 1)}>
+            <IconButton sx={{ color: '#4e5b73' }} component="span" onClick={() => number > 0.25 && setNumber(number - 0.25)}>
                 <IndeterminateCheckBox />
             </IconButton>
             <InputBase sx={{ border: 1, borderColor: '#4e5b73', width: '6ch', px: 1 }}
@@ -17,21 +18,24 @@ function UnitSelect({ number, setNumber }) {
                 value={number}
                 onChange={(e) => {
                     const tmp = parseInt(e.target.value)
-                    setNumber(tmp ? (tmp < 999 ? tmp : 999) : 0)
+                    setNumber(tmp ? (tmp < 999 ? tmp : 999) : 0.25)
+                    
                 }}
             >
             </InputBase>
-            <IconButton sx={{ color: '#4e5b73' }} component="span" onClick={() => number < 999 && setNumber(number + 1)}>
+            <IconButton sx={{ color: '#4e5b73' }} component="span" onClick={() => number < 999 && setNumber(number + 0.25)}>
                 < AddBox />
             </IconButton>
         </React.Fragment >
     )
 }
+
 export default function ProductInfoSection() {
-    const [number, setNumber] = useState(0)
+    const [number, setNumber] = useState(0.25)
+    
     const {productId} = useParams()
     const thisProduct = productData.find(prod => prod.id === productId)
-
+    const total = number*thisProduct.price
     return (     
         <div>
             <h1>{thisProduct.name}</h1>
@@ -42,12 +46,15 @@ export default function ProductInfoSection() {
             <h4>Chiều rộng: {thisProduct.width}cm</h4>
             <h4>Cân nặng: {thisProduct.weight}gsm</h4>
             <UnitSelect number={number} setNumber={setNumber} />
-            
+            <h2 style={{display: 'inline'}}>{number} mét</h2>
             <br></br>
-            <Button variant="contained" sx={{mt:2, p:2, backgroundColor:"#384257"}}>
+            <div style={{margin:'10px'}}>
+            <Button variant="contained" sx={{p:2,backgroundColor:"#384257"}}>
                 Thêm vào giỏ hàng
             </Button>
             
+            <h2 style={{display: 'inline', padding: '10px'}}>{total} đồng</h2>
+            </div>
         </div>
     );
 }
