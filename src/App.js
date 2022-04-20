9001bad65d2dff9a0992ce4f9941830821243a70
Trình Huỳnh Thiên <trinh.huynhcollector@hcmut.edu.vn>
2022-04-20 16:48:48 +0700

    shipment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
@@ -6,17 +5,14 @@ import MenuPage from "./pages/MenuPage";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
 import CartPage from "./pages/CartPage";
-import ProductPage from "./pages/ProductPage"
-import AccountPage from "./pages/AccountPage"
+import ProductPage from "./pages/ProductPage";
+import AccountPage from "./pages/AccountPage";
 import AccountOrderPage from "pages/AccountOrderPage";
-import AddressPage from "pages/AccountAddressPage"
-import PaymentPage from "pages/PaymentPage"
-import ForgotPasswordPage from "./pages/ForgotPasswordPage"
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import AddressPage from "pages/AccountAddressPage";
+import PaymentPage from "pages/PaymentPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ShipmentPage from "./pages/ShipmentPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 
 function App() {
@@ -34,7 +30,12 @@ function App() {
           <Route path="/account" element={<AccountPage />} />
           <Route path="/account/address" element={<AddressPage />} />
           <Route path="/payment" element={<PaymentPage />} />
-          <Route path="/account/order/:orderId" element={<AccountOrderPage />} />
+          <Route path="/shipment" element={<ShipmentPage />} />
+          <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
+          <Route
+            path="/account/order/:orderId"
+            element={<AccountOrderPage />}
+          />
           <Route path="/menu/:productId" element={<ProductPage />} />
         </Routes>
       </Router>
