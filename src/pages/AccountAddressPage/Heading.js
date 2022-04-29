7bd89dbf1ea0acebeb79ee122959e5419954b24d
Trình Huỳnh Thiên <trinh.huynhcollector@hcmut.edu.vn>
2022-04-30 02:15:30 +0700

    done address

--- src/pages/AccountAddressPage/Heading.js
+++ src/pages/AccountAddressPage/Heading.js
@@ -2,29 +2,25 @@ import { Breadcrumbs, Typography, Link, Box } from "@mui/material";
 import React from "react";
 
 export default function Heading() {
-    return (
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', my: 2 }}>
-            <h2>Địa chỉ</h2>
-            <Breadcrumbs aria-label="breadcrumb">
-                <Link
-                    underline="hover"
-                    color="inherit"
-                    href="/"
-                >
-                    Trang chủ
-                </Link>
-                <Link
-                    underline="hover"
-                    color="inherit"
-                    href="/"
-                >
-                   Tài khoản
-                </Link>
-                <Typography color="#0f0d0c">
-                    Địa chỉ
-                </Typography>
-            </Breadcrumbs>
-        </Box>
-
-    )
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        my: 2,
+      }}
+    >
+      <h2>Địa chỉ</h2>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link underline="hover" color="inherit" href="/">
+          Trang chủ
+        </Link>
+        <Link underline="hover" color="inherit" href="/account">
+          Tài khoản
+        </Link>
+        <Typography color="#0f0d0c">Địa chỉ</Typography>
+      </Breadcrumbs>
+    </Box>
+  );
 }
--- src/pages/AccountAddressPage/index.js
+++ src/pages/AccountAddressPage/index.js
@@ -9,7 +9,7 @@ import qs from "qs";
 
 export default function AccountAddressPage() {
   const [contacts, setContacts] = useState(addressList);
-  const [edit, setEdit] = useState(0);
+  const [edit, setEdit] = useState(null);
   const deleteContact = (key) => () => {
     console.log("Delete");
     const { [key]: _, ...newContacts } = contacts;
