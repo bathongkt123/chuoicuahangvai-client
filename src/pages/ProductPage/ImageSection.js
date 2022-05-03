0b05cde249ea5bb139ea6131e54e700223b03b77
Trình Huỳnh Thiên <trinh.huynhcollector@hcmut.edu.vn>
2022-05-03 23:03:48 +0700

    fix images

--- src/pages/ProductPage/ImageSection.js
+++ src/pages/ProductPage/ImageSection.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@material-ui/core/Box";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
-export default function ImageSection({ productImages }) {
+export default function ImageSection({
+  productImages,
+  largeImage,
+  setLargeImage,
+}) {
   const renderSlides = () =>
-    productImages
-      .slice(1)
-      .map((item) => (
-        <Box
-          key={item}
-          component="img"
-          src={`${process.env.REACT_APP_STRAPI_URL}${item}`}
-        />
-      ));
+    productImages.map((item) => (
+      <Box
+        onClick={() => setLargeImage(item)}
+        key={item}
+        component="img"
+        src={`${process.env.REACT_APP_STRAPI_URL}${item}`}
+      />
+    ));
   return (
     <Box>
       <Box
         component="img"
-        src={`${process.env.REACT_APP_STRAPI_URL}${productImages[0]}`}
+        src={`${process.env.REACT_APP_STRAPI_URL}${largeImage}`}
         loading="lazy"
         sx={{
           display: "flex",
@@ -28,13 +31,7 @@ export default function ImageSection({ productImages }) {
         }}
       />
       <Box sx={{ mt: 4 }}>
-        <Slider
-          dots={false}
-          slidesToShow={2}
-          slidesToScroll={2}
-          autoplay={true}
-          autoplaySpeed={4000}
-        >
+        <Slider dots={false} slidesToShow={2} slidesToScroll={2}>
           {renderSlides()}
         </Slider>
       </Box>
